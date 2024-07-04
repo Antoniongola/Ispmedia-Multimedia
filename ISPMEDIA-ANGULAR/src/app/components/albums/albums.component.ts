5fdcb9d1a8944711f54f54052d88dcb3ee62f820
antoniongola <antoniongola10@gmail.com>
2024-07-04 19:08:35 +0100

    album review updated and fixed.

--- ISPMEDIA-ANGULAR/src/app/components/albums/albums.component.ts
+++ ISPMEDIA-ANGULAR/src/app/components/albums/albums.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {AlbumService} from "../../services/album/album.service";
 import {Album} from "../../entities/Album";
+import {ArtistaService} from "../../services/artista/artista.service";
+import {Artista} from "../../entities/Artista";
 
 @Component({
   selector: 'app-albums',
@@ -10,16 +12,31 @@ import {Album} from "../../entities/Album";
 export class AlbumsComponent implements OnInit{
   albums : Album[] = [];
   albumImages: { [key: string]: any } = {};
+  finalAlbums : Album[] = [];
+  artistas:Artista[]=[];
 
-  constructor(private albumService : AlbumService){
+  constructor(private albumService : AlbumService,
+              private artistaService:ArtistaService){
+  }
+
+  ngOnInit(){
     this.albumService.getAlbums().subscribe(response=>{
       this.albums = response;
+      this.artistaService.getArtistas().subscribe(response=>{
+        this.artistas=response;
+      });
+
+      this.albums.forEach(album=>{
+        this.artistas.forEach(artista=>{
+          if(artista.albums.includes(album)){
+            console.log('Dono do album '+album.titulo+', '+artista.titulo);
+          }
+        })
+      });
+
       this.albumService.loadImages(this.albums, this.albumImages);
     }, error=>{
       console.log('Erro nos albuns: '+error);
     })
-  }
-  ngOnInit(){
-
   }
 }
